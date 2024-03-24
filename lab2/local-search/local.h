10c79385c20f4fa476917325a796d8b79054bdad
majsterkovic <mariusz8h@o2.pl>
2024-03-24 10:25:43 +0100

    Refactor code

--- lab2/local-search/local.h
+++ lab2/local-search/local.h
@@ -5,8 +5,35 @@
 #ifndef LOCAL_SEARCH_LOCAL_H
 #define LOCAL_SEARCH_LOCAL_H
 
+#define REVERSE_SUBSEQUENCE 0
+#define SWAP_NODES 1
+
+#include "Utils.h"
+
 
 class local {
+public:
+    static std::vector<std::pair<int, std::pair<int, int> > > generate_intra_route_inner(const std::vector<int>& cycle);
+
+    static std::vector<int> local_search_steepest_inner_cycle(std::vector<int> starting_cycle, std::vector<std::vector<int> > &dist_mat);
+
+    static std::pair<std::vector<int>, std::vector<int>> local_search_steepest_inner(std::vector<int> &cycle_1, std::vector<int> &cycle_2, std::vector<std::vector<int> > &dist_mat);
+
+    static std::vector<int> local_search_greedy_inner_cycle(std::vector<int> starting_cycle, std::vector<std::vector<int> > &dist_mat);
+
+    static std::pair<std::vector<int>, std::vector<int>> local_search_greedy_inner(std::vector<int> &cycle_1, std::vector<int> &cycle_2, std::vector<std::vector<int> > &dist_mat);
+
+    static std::pair<std::vector<int>, std::vector<int> > swap_nodes_between(std::vector<int> cycle1, std::vector<int> cycle2, int pos_a, int pos_b);
+
+    static std::pair<std::vector<int>, std::vector<int>> local_search_steepest_between(std::vector<int> cycle_1, std::vector<int> cycle_2, std::vector<std::vector<int> > &dist_mat);
+
+    static std::pair<std::vector<int>, std::vector<int>> local_search_greedy_between(std::vector<int> cycle_1, std::vector<int> cycle_2, std::vector<std::vector<int> > &dist_mat);
+
+    static std::vector<int> random_search_cycle(std::vector<int> &starting_cycle, std::vector<std::vector<int> > &dist_mat, std::chrono::microseconds time_limit_microseconds);
+
+    static std::pair<std::vector<int>, std::vector<int>> random_search(std::vector<int> &cycle_1, std::vector<int> &cycle_2, std::vector<std::vector<int> > &dist_mat, int time_limit=100000);
+
+
 
 };
 
---
+++ lab2/report.md
@@ -0,0 +1,31 @@
+## Sprawozdanie z laboratorium nr 2
+# Inteligentne Metody Optymalizacji
+
+Autorzy: Jakub Gołąb, Mariusz Hybiak
+
+### Wprowadzenie
+Celem zadania była implementacja algorytmów lokalnego przeszukiwania w wersjach stromej (steepest) izachłannej (greedy), z dwoma różnym rodzajami sąsiedztwa, starując albo z rozwiązań losowych, albo z rozwiązań uzyskanych za pomocą jednej z heurystyk opracowanych w ramach poprzedniego zadania.
+
+### Algorytmy
+
+### Wyniki eksperymentu obliczeniowego
+
+W tabeli przedstawiono sumy długości cykli dla każdej z metod dla obu instancji problemu.
+
+| Instancja   | Metoda            | Średnia (min – max) [jednostki odległości]    |
+|-------------|-------------------|-----------------------------|
+
+
+
+### Wizualizacje najlepszych rozwiązań
+
+| Instancja  | 
+
+
+### Wnioski
+
+- 
+
+### Kod Programu
+
+Kod programu znajduje się pod [tym linkiem](https://github.com/majsterkovic/imo/blob/main/lab2/) w pliku cpp.
